25a73564dd812ca94c25aa10792d11b64b46f695
HieuTNgoc <hieutnhe153199@fpt.edu.vn>
2022-04-26 17:22:02 +0700

    first commit

---
+++ app/config/config.php
@@ -0,0 +1,16 @@
+<?php
+	// Database params
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'root');
+	define('DB_PASS', '');
+	define('DB_NAME', 'base_account');
+
+	//APPROOT
+	define('APPROOT', dirname(dirname(__FILE__)));
+
+	//URLROOT (Dynamic links)
+	define('URLROOT', 'localhost/mvc_framework');
+
+	//Sitename 
+	define('SITENAME', 'BASE Account');
+?>
