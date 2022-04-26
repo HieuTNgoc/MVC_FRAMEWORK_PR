254fb41ac01c4eb87ab31e7781db90e9e1db7d5a
Tran Hieu <hieutnhe153199@fpt.edu.vn>
2022-04-27 01:50:41 +0700

    Finish MVC Framework

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -3,12 +3,15 @@ class Pages extends Controller
 {
 	public function __construct()
 	{
+		$this->userModel = $this->model('User');
 	}
 	public function index()
 	{
+		$users = $this->userModel->getUsers();
+
 		$data = [
 			'title' => 'home page',
-			'name' => 'Hieu'
+			'users' => $users
 		];
 
 		$this->view('pages/index', $data);
--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,3 +1,6 @@
-<?php 
-	echo SITENAME;
+<?php
+    foreach ($data['users'] as $user) {
+        echo "Information: " . $user->user_name . '  ' . $user->user_email;
+        echo '<br>';
+    }
 ?>
