aea383a9265b94253b1d4a4e55c30d6635443fb4
HieuTNgoc <hieutnhe153199@fpt.edu.vn>
2022-05-04 17:27:26 +0700

    start code ajax

--- app/views/users/login.php
+++ app/views/users/login.php
@@ -8,6 +8,8 @@
     <title>Login - Base Account</title>
     <link rel="stylesheet" href="./public/css/style.css">
     <link rel="stylesheet" href="./public/css/login.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="./public/js/javascript.js"></script>
 </head>
 
 <body>
