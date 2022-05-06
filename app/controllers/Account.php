5d974a6d53ad822f50710b2d61b4de67d9659715
HieuTNgoc <hieutnhe153199@fpt.edu.vn>
2022-05-06 17:23:09 +0700

    load user data update

--- app/controllers/Account.php
+++ app/controllers/Account.php
@@ -13,8 +13,31 @@ class Account extends Controller
 	public function account()
 	{
 		$data = [
-			'title' => 'Account - Base Account'
+			'username' => '',
+			'email' => '',
+			'first_name' => '',
+			'last_name' => '',
+			'address' => '',
+			'phone' => '',
+			'img_url' => '',
+			'dob' => '',
+			'position' => ''
 		];
+
+		$user = $this->userModel->loadUserData($_SESSION['user_id']);
+		$data['username'] = $user->username;
+		$data['email'] = $user->email;
+		$data['first_name'] = $user->first_name;
+		$data['last_name'] = $user->last_name;
+		$data['address'] = $user->address;
+		$data['phone'] = $user->phone;
+		$data['img_url'] = $user->img_url;
+		$data['dob'] = $user->dob;
+		$data['position'] = $user->position;
+
+		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+		}
 
 		$this->view('users/account', $data);
 	}
